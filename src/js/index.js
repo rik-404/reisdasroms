4bb7748e356841f90d251ec82e587969606a4928
rik-404 <ricardovendramini.contato@gmail.com>
2025-01-26 02:07:58 -0300

    adicionado pagina emuladores

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+alert('Este site foi criado exclusivamente para fins de estudo e aprendizado de HTML e CSS. Não contém informações ou conteúdos piratas. Todo o conteúdo aqui apresentado é fictício e destinado apenas para prática e desenvolvimento de habilidades em web design')
